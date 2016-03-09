2b3b00bf0c695ee4d1666ada308bb684d123baa4
Liem Ly Quan <liem.ly@cangroup.io>
2016-03-09 20:39:07 +0700

    Bonus 1

--- Instagram/PhotosViewController.swift
+++ Instagram/PhotosViewController.swift
@@ -30,47 +30,50 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  func fetchData() {
+    // Do any additional setup after loading the view, typically from a nib.
+    let clientId = "e05c462ebd86446ea48a5af73769b602"
+    let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+    let request = NSURLRequest(URL: url!)
+    let session = NSURLSession(
+      configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+      delegate:nil,
+      delegateQueue:NSOperationQueue.mainQueue()
+    )
     
-    func fetchData() {
-        // Do any additional setup after loading the view, typically from a nib.
-        let clientId = "e05c462ebd86446ea48a5af73769b602"
-        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
-        let request = NSURLRequest(URL: url!)
-        let session = NSURLSession(
-            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
-            delegate:nil,
-            delegateQueue:NSOperationQueue.mainQueue()
-        )
-        
-        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
-            completionHandler: { (dataOrNil, response, error) in
-                if let data = dataOrNil {
-                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
-                        data, options:[]) as? NSDictionary {
-                            self.mediaData = responseDictionary
-                            self.mainTableView.reloadData()
-                    }
-                }
-        });
-        task.resume()
-
-    }
+    let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+      completionHandler: { (dataOrNil, response, error) in
+        if let data = dataOrNil {
+          if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+            data, options:[]) as? NSDictionary {
+              self.mediaData = responseDictionary
+              self.mainTableView.reloadData()
+          }
+        }
+    });
+    task.resume()
+    
+  }
   
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    if let data = mediaData {
-      return (data["data"]?.count)!
-    }
-    return 0
+//    if let data = mediaData {
+//      return (data["data"]?.count)!
+//    }
+//    return 0
+    return 1
   }
   
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("PhotoCell", forIndexPath: indexPath) as! PhotoCellController
     
-    let photoUrl = mediaData!["data"]?[indexPath.row]["images"]!!["standard_resolution"]!!["url"] as! String
+    let photoUrl = mediaData!["data"]?[indexPath.section]["images"]!!["standard_resolution"]!!["url"] as! String
     
     cell.photoView.setImageWithURL(NSURL(string: photoUrl)!)
     return cell
   }
+  
+  
   
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     let vc = segue.destinationViewController as! PhotoDetailsViewController
@@ -83,31 +86,64 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     mainTableView.deselectRowAtIndexPath(indexPath, animated: true)
   }
   
-    func refreshControlAction(refreshControl: UIRefreshControl) {
-        let clientId = "e05c462ebd86446ea48a5af73769b602"
-        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
-        let request = NSURLRequest(URL: url!)
-        let session = NSURLSession(
-            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
-            delegate:nil,
-            delegateQueue:NSOperationQueue.mainQueue()
-        )
-        
-        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
-            completionHandler: { (dataOrNil, response, error) in
-                if let data = dataOrNil {
-                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
-                        data, options:[]) as? NSDictionary {
-                            self.mediaData = responseDictionary
-                            self.mainTableView.reloadData()
-                            refreshControl.endRefreshing()
-                    }
-                }
-        });
-        task.resume()
-        
-    }
+  func refreshControlAction(refreshControl: UIRefreshControl) {
+    let clientId = "e05c462ebd86446ea48a5af73769b602"
+    let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+    let request = NSURLRequest(URL: url!)
+    let session = NSURLSession(
+      configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+      delegate:nil,
+      delegateQueue:NSOperationQueue.mainQueue()
+    )
+    
+    let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+      completionHandler: { (dataOrNil, response, error) in
+        if let data = dataOrNil {
+          if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+            data, options:[]) as? NSDictionary {
+              self.mediaData = responseDictionary
+              self.mainTableView.reloadData()
+              refreshControl.endRefreshing()
+          }
+        }
+    });
+    task.resume()
+    
+  }
   
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        if let data = mediaData {
+          return (data["data"]?.count)!
+        }
+        return 0
+  }
+  
+  func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    let headerView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+    headerView.backgroundColor = UIColor(white: 1, alpha: 0.9)
+    
+    let profileView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 30))
+    profileView.clipsToBounds = true
+    profileView.layer.cornerRadius = 15
+    profileView.layer.borderColor = UIColor(white: 0.7, alpha: 0.8).CGColor
+    profileView.layer.borderWidth = 1
+    let profileUrlString = mediaData!["data"]?[section]["user"]!!["profile_picture"] as! String
+    profileView.setImageWithURL(NSURL(string: profileUrlString)!)
+    headerView.addSubview(profileView)
+    
+    let usernameLabel = UILabel(frame: CGRect(x: 50, y: 10, width: 200, height: 30))
+    usernameLabel.text = mediaData!["data"]?[section]["user"]!!["username"] as! String
+    headerView.addSubview(usernameLabel)
+    
+    
+    
+    return headerView
+  }
+  
+  func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return CGFloat(50)
+  }
+
   
 }
 
