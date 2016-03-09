8ce2dd66404f2171aadf8b6ac3231be231cc6e3f
RyanDam <py.ryan.dam@gmail.com>
2016-03-09 20:19:42 +0700

    milestone 5

--- Instagram/PhotosViewController.swift
+++ Instagram/PhotosViewController.swift
@@ -19,34 +19,42 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     mainTableView.delegate = self
     mainTableView.dataSource = self
     
-    // Do any additional setup after loading the view, typically from a nib.
-    let clientId = "e05c462ebd86446ea48a5af73769b602"
-    let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
-    let request = NSURLRequest(URL: url!)
-    let session = NSURLSession(
-      configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
-      delegate:nil,
-      delegateQueue:NSOperationQueue.mainQueue()
-    )
+    fetchData()
     
-    let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
-      completionHandler: { (dataOrNil, response, error) in
-        if let data = dataOrNil {
-          if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
-            data, options:[]) as? NSDictionary {
-              self.mediaData = responseDictionary
-              NSLog("response: \(responseDictionary)")
-              self.mainTableView.reloadData()
-          }
-        }
-    });
-    task.resume()
+    let refreshControl = UIRefreshControl()
+    refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+    mainTableView.insertSubview(refreshControl, atIndex: 0)
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+    
+    func fetchData() {
+        // Do any additional setup after loading the view, typically from a nib.
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            self.mediaData = responseDictionary
+                            self.mainTableView.reloadData()
+                    }
+                }
+        });
+        task.resume()
+
+    }
   
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     if let data = mediaData {
@@ -75,6 +83,30 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     mainTableView.deselectRowAtIndexPath(indexPath, animated: true)
   }
   
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            self.mediaData = responseDictionary
+                            self.mainTableView.reloadData()
+                            refreshControl.endRefreshing()
+                    }
+                }
+        });
+        task.resume()
+        
+    }
   
   
 }
